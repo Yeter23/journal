0509520f735f75eb8d786552cacb0b18fbb3bb37
Yeter23 <yeteryusifova00@gmail.com>
2024-11-24 02:07:49 +0400

    journal proyektini yaratdiq

---
+++ projects/journal/src/test/java/com/yeter/journal/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeter.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
